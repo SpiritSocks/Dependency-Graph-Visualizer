356a189a94cb7f85a2f15cf44acf94db4efdb5f0
Valera <vdteterich@gmail.com>
2025-10-27 01:48:55 +0300

    Working on stage 1

---
+++ Models/DependecyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DependencyGraphVisualization.Models
+{
+    [XmlRoot("Configuration")]
+    public class DependecyData
+    {
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public bool TestMode { get; set; }
+        public string? Version { get; set; }
+        public string? OutputFile { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using DependencyGraphVisualization.Models;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace DependencyGraphVisualization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string?> dataPair = new Dictionary<string, string?>();
+
+            string filePath = Path.Combine(GetProjectRoot(), "XML_Files", "TestDependencyFile.xml");
+
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(filePath))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(DependecyData));
+
+                    var data = (DependecyData)serializer.Deserialize(reader)!;
+                    
+                    dataPair.Add("Name", data.Name);
+                    dataPair.Add("URL", data.Url);
+                    dataPair.Add("Test-mode", data.TestMode.ToString());
+                    dataPair.Add("Version", data.Version);
+                    dataPair.Add("Output-file-name", data.OutputFile); 
+                }
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine($"XML Error: {ex.Message}");
+                Console.WriteLine($"Line: {ex.LineNumber}, Position: {ex.LinePosition}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Deserialization error: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+
+            foreach (var pair in dataPair)
+            {
+                if (string.IsNullOrEmpty(pair.Value))
+                {
+                    throw new FormatException("Value cannot be null or empty");
+                }
+
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+
+        public static string GetProjectRoot()
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDir);
+
+            while (directory != null && !directory.GetFiles("*.csproj").Any())
+            {
+                directory = directory.Parent!;
+            }
+
+            return directory?.FullName ?? currentDir;
+        }
+
+    }
+}
